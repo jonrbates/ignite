2f04b489e3a54f9c53b02d576c1ba5f0105173ea
jonrbates <jonrbates@gmail.com>
2020-05-10 22:06:10 -0700

    Make network output compatible with CrossEntropyLoss

--- examples/contrib/mnist/mnist_with_tensorboard_logger.py
+++ examples/contrib/mnist/mnist_with_tensorboard_logger.py
@@ -53,7 +53,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=-1)
+        return x
 
 
 def get_data_loaders(train_batch_size, val_batch_size):
